d0a9a12dadf36bef6d86f9573d395b78442e7e67
JonathanWaller <waller.j27@gmail.com>
2018-12-10 13:01:35 -0600

    imported scss

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Signup.scss";
+
+class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <div className="test">Hello from Signup</div>
+      </>
+    );
+  }
+}
+
+export default Signup;
