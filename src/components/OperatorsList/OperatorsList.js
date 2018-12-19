5c071eae99e37e797b1f9a9380be2ad5e9d1b879
JonathanWaller <waller.j27@gmail.com>
2018-12-18 21:54:21 -0600

    listing operators in list

--- src/components/OperatorsList/OperatorsList.js
+++ src/components/OperatorsList/OperatorsList.js
@@ -2,14 +2,35 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class OperatorsList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      operators: []
+    };
+  }
   componentDidMount() {
     axios.get("api/operators").then(response => {
-      console.log(response);
+      //   console.log(response);
+      this.setState({ operators: response.data });
     });
   }
 
   render() {
-    return <div>hello from the Listtt</div>;
+    console.log(this.state);
+    let operatorsList = this.state.operators.map((operator, id) => {
+      return (
+        <div key={id}>
+          <div>{operator.name_last}</div>
+          <div>{operator.name_first}</div>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <h1>hello from the Listtt</h1>
+        {operatorsList}
+      </div>
+    );
   }
 }
 export default OperatorsList;
